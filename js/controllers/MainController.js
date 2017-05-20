720fa272f2e4ffb4e7653c1459c4b3e05a88791f
Lailson Azevedo <lailson93@gmail.com>
2017-05-20 17:41:32 -0300

    ng-clickII

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -8,34 +8,42 @@ app.controller('MainController',['$scope',function($scope){
         price: 19, 
         pubdate: new Date('2014', '03', '08'), 
         cover: 'img/the-book-of-trees.jpg' ,
-        likes:0
+        likes:0,
+        dislikes:0
       }, 
       { 
         name: 'Program or be Programmed', 
         price: 8, 
         pubdate: new Date('2013', '08', '01'), 
         cover: 'img/program-or-be-programmed.jpg'  ,
-        likes:0
+        likes:0,
+        dislikes:0
       } ,
     
-    	{ 
+      { 
         name: 'Livro1', 
         price: 55, 
         pubdate: new Date('2014', '08', '09'), 
         cover: 'img/program-or-be-programmed.jpg' ,
-        likes:0 
+        likes:0,
+        dislikes:0
       } ,
-    	{ 
+      { 
         name: 'Livro2', 
         price: 22, 
         pubdate: new Date('2012', '08', '01'), 
         cover: 'img/program-or-be-programmed.jpg' ,
-        likes:0
+        likes:0,
+        dislikes:0
       } 
     
-	  ]
-	$scope.plusOne = function(index){
+    ]
+  $scope.plusOne = function(index){
     $scope.products[index].likes +=1;
+  }
+  
+  $scope.minusOne = function(index){
+    $scope.products[index].dislikes +=1;
   }
   
 }]);
